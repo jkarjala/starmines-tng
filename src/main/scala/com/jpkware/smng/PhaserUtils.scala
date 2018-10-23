9770dc3f829bd5d9603f7384ff42c881e4b5961b
Jari Karjala <jka@iki.fi>
2018-10-23 19:47:02 +0300

    Explosions behind scorebox, all caps TNG

--- src/main/scala/com/jpkware/smng/PhaserUtils.scala
+++ src/main/scala/com/jpkware/smng/PhaserUtils.scala
@@ -4,13 +4,22 @@ import scala.scalajs.js
 
 // This state is the way scala.js models a Javascript literal object -> https://www.w3schools.com/js/js_objects.asp
 object State {
-  def apply(preload: () => Unit,  create: () => Unit, update: () => Unit, render: () => Unit): State = {
-    js.Dynamic.literal(preload = preload, create = create, update = update, render = render).asInstanceOf[State]
+  def apply(init: () => Unit, preload: () => Unit,  create: () => Unit, update: () => Unit, render: () => Unit): State = {
+    js.Dynamic.literal(init = init, preload = preload, create = create, update = update, render = render).asInstanceOf[State]
   }
+  def apply(preload: () => Unit,  create: () => Unit, update: () => Unit, render: () => Unit): State = {
+    js.Dynamic.literal(init = () => {}, preload = preload, create = create, update = update, render = render).asInstanceOf[State]
+  }
+
+  def apply(preload: () => Unit,  create: () => Unit, update: () => Unit): State = {
+    js.Dynamic.literal(init = () => {}, preload = preload, create = create, update = update, render = () => {}).asInstanceOf[State]
+  }
+
 }
 
 @js.native
 trait State extends js.Object {
+  def init: Unit = js.native
   def preload: Unit = js.native
   def create: Unit = js.native
   def update: Unit = js.native
